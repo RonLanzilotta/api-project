1b3ee0497899f8ed15679b3b80dbca6b2268ddaa
Ron Lanzilotta <rlanzilotta1010@gmail.com>
2022-12-19 12:54:34 -0500

    get by date is working

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 import express from 'express';
 import connection from './db/connection.js';
-import Apod from './models/Apod.js'
+import Apod from './models/Apod.js';
 
 const app = express();
 
 app.listen(3000, () => {
     console.log('Listening on 3000')
-})
+});
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.redirect("http://localhost:3000/apod")
-})
+});
 
 app.get("/apod", async (req, res) => {
     const apod = await Apod.find({});
     res.json(apod);
+});
+
+app.get("/apod/:date", async (req, res) => {
+    //date must be YYYY-MM-DD format, between 11/30/22 -- 12/19/22 range
+    const apodByDate = await Apod.find({ date: req.params.date });
+    res.json(apodByDate);
 })
